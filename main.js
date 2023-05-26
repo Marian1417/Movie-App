28017e619b1c1e5137301b2d17b35b3d6e2f4b20
Eunz27 <eunicrisambos272@gmail.com>
2023-05-26 21:02:59 +0800

    finish fix-1

--- main.js
+++ main.js
@@ -4,48 +4,88 @@ const movieNameRef = document.getElementById("movie-name");
 const searchBtn = document.getElementById("search-btn");
 const result = document.getElementById("result");
 
-//function
-const getMovie = () => {
-  const movieName = movieNameRef.value;
-  const key = "626ff0f7";
-  const url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
-  //if input field is empty
-  if (movieName.length <= 0) {
-    result.innerHTML = `<h3 class="msg">Please enter a movie title</h3`;
+const key = "626ff0f7";
+const baseUrl = "http://www.omdbapi.com/";
+
+const displayErrorMessage = (message) => {
+  result.innerHTML = `<h3 class="msg">${message}</h3>`;
+};
+
+const searchMovies = async (searchTerm) => {
+  if (!searchTerm) {
+    displayErrorMessage("Please enter a search term");
+    return;
   }
-  //if input field is not empty
-  else {
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((data) => {
-        result.innerHTML = `
-        <div class="info">
-           <img src=${data.Poster} class="poster">
-           <div>
-               <h2>${data.Title}</h2>
-               <div class="details">
-              <span>${data.Rated}</span>
-               <span>${data.Year}</span>
-           </div> 
-           <div class="genre">
-             <div>${data.Genre.split(",").join()}</div>
-           </div>
-           <h3>Plot</h3>
-           <p>${data.Plot}</p>
-           <h3>Cast</h3>
-           <p>${data.Actor}</p>
-              
-        
-           
-          
-           
-           
-        </div>
 
+  searchBtn.disabled = true;
+  movieNameRef.disabled = true;
 
-        `;
-      });
+  const url = `${baseUrl}?s=${searchTerm}&apikey=${key}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.Response === "False") {
+      displayErrorMessage(data.Error);
+    } else {
+      const movies = data.Search;
+      const movieDetails = [];
+
+      for (let i = 0; i < movies.length; i++) {
+        const movieUrl = `${baseUrl}?i=${movies[i].imdbID}&apikey=${key}`;
+        const movieResponse = await fetch(movieUrl);
+        const movieData = await movieResponse.json();
+
+        if (movieData.Response === "True") {
+          movieDetails.push(movieData);
+        }
+      }
+
+      if (movieDetails.length === 0) {
+        displayErrorMessage("No results found");
+      } else {
+        result.innerHTML = movieDetails
+          .map(
+            (movie) => `
+            <div class="info">
+              <img src=${movie.Poster} class="poster">
+              <div>
+                <h2>${movie.Title}</h2>
+                <div class="details">
+                  <span>${movie.Rated}</span>
+                  <span>${movie.Year}</span>
+                </div> 
+                <div class="genre">
+                  <div>${movie.Genre.split(",").join()}</div>
+                </div>
+                <h3>Plot</h3>
+                <p>${movie.Plot}</p>
+                <h3>Cast</h3>
+                <p>${movie.Actors}</p>
+              </div>
+            </div>
+          `
+          )
+          .join("");
+      }
+    }
+  } catch (error) {
+    displayErrorMessage("An error occurred. Please try again later.");
+  } finally {
+    searchBtn.disabled = false;
+    movieNameRef.disabled = false;
   }
 };
-searchBtn.addEventListener("click", getMovie);
-window.addEventListener("load", getMovie);
+
+searchBtn.addEventListener("click", () => {
+  const searchTerm = movieNameRef.value;
+  searchMovies(searchTerm);
+});
+
+movieNameRef.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) {
+    const searchTerm = movieNameRef.value;
+    searchMovies(searchTerm);
+  }
+});
